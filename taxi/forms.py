6b9449d004f3ea12c32a0878be9022ac8a6d52df
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-10-21 09:49:28 +0300

    'Solution'

--- taxi/forms.py
+++ taxi/forms.py
@@ -35,24 +35,6 @@ class DriverLicenseUpdateForm(DriverForm):
         model = Driver
         fields = ("license_number",)
 
-    # def clean_license_number(self):
-    #
-    #     license_number = self.cleaned_data["license_number"]
-    #     print(license_number[3].upper())
-    #     if not len(license_number) == 8:
-    #         raise ValidationError("Length must be 8 symbols")
-    #     if not license_number[0:3].upper() == license_number[0:3]:
-    #         raise ValidationError("The first 3 symbols must be in upper case letters")
-    #     first_part = license_number[0:3]
-    #     if len ([el for el in first_part if not el.isalpha()]) > 0:
-    #         raise ValidationError ("The first 3 symbols must be in upper case letters")
-    #     end_part = license_number[-5:]
-    #     if len([el for el in end_part if not el.isdigit()]) > 0:
-    #         raise ValidationError("Last 5 symbols must be as digit")
-    #
-    #     return license_number
-
-
 
 class CarForm(forms.ModelForm):
     drivers = forms.ModelMultipleChoiceField(
