0a9a92f0b1d5bc77955ad7786ad7e572221ba1b3
stoveprofi <stoveprofi@gmail.com>
2022-10-24 17:31:53 +0100

    Corrected

--- taxi/forms.py
+++ taxi/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-from config import constant
+from taxi import constant
 from taxi.models import Driver, Car
 
 
