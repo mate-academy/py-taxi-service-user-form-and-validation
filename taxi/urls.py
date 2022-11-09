2c4101814f07e3d6fdd0dcf620a2bd021fe1e50e
Iurii Lobunets <iurii@lobunets.com.ua>
2022-11-09 22:07:45 +0200

    Solution of py-taxi-service-user-form-and-validation

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,10 +9,14 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverLicenseUpdateView,
+    DriversDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    car_update_driver
 )
 
 urlpatterns = [
@@ -41,10 +45,26 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path(
+        "cars/<int:pk>/updatedriver/",
+        car_update_driver,
+        name="car-update-driver"
+    ),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/updatelicense/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update-license"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriversDeleteView.as_view(),
+        name="driver-delete"
     ),
 ]
 
