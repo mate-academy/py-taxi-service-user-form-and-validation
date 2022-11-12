e3e4762065987fdab71aeae0d6450be8962619df
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-11-12 23:35:20 +0200

    'py_taxi_service_user_form_validation_'

--- taxi/views.py
+++ taxi/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import DriverLicenseUpdateForm, CarCreationForm
+from .forms import DriverLicenseUpdateForm, CarCreationForm, DriverCreationForm
 from .models import Driver, Car, Manufacturer
 
 
@@ -104,7 +104,7 @@ class DriverDetailView(LoginRequiredMixin, generic.DetailView):
 
 class DriverCreateView(LoginRequiredMixin, generic.CreateView):
     model = Driver
-    fields = "__all__"
+    form_class = DriverCreationForm
     success_url = reverse_lazy("taxi:driver-list")
 
 
