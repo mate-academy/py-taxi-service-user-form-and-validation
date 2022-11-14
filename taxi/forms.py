f4775a9e743256d8de8b60516b7425c5ca7e0188
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-14 22:02:21 +0200

    Fixed bags namber 6

--- taxi/forms.py
+++ taxi/forms.py
@@ -33,17 +33,15 @@ class DriverUpdateLicenseForm(forms.ModelForm):
                 f"{DriverUpdateLicenseForm.LEN_LICENSE_NUMBER}"
             )
 
-        if not license_number[:3].isalpha():
-            raise ValidationError(
-                "First 3 characters must be letters"
-            )
-
-        if not license_number[:3].isupper():
+        elif (
+                not license_number[:3].isupper()
+                or not license_number[:3].isalpha()
+        ):
             raise ValidationError(
                 "First 3 characters must be uppercase letters"
             )
 
-        if not license_number[3:].isdigit():
+        elif not license_number[3:].isdigit():
             raise ValidationError(
                 "Last 5 characters must be digits"
             )
