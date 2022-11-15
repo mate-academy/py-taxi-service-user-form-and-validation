218e1de44cdf49018b8c7a27777fa8c9e20d3fa7
Zakhar Podporinov <zp162162@gmail.com>
2022-11-15 20:27:18 +0200

    Update validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -10,16 +10,17 @@ class LicenseValidateMixin:
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
 
-        if not (
-            len(license_number) == 8
-            and license_number[0:3].isupper()
-            and license_number[0:3].isalpha()
-            and license_number[3:].isnumeric()
-        ):
+        if not len(license_number) == 8:
+            raise ValidationError("length is not equal to 8")
+
+        if not (license_number[0:3].isalpha()
+                and license_number[0:3].isupper()):
             raise ValidationError(
-                "The driver's license must be in the format 'XXX00000' "
-                "(3 uppercase letters and 5 numbers)"
+                "the first 3 symbols are not uppercase letters"
             )
+
+        if not license_number[3:].isnumeric():
+            raise ValidationError("the last 5 characters are not numbers")
 
         return license_number
 
