8d96d4eda355b07a61e08de6a625dec7480d78c5
Vika <viktorykoroliova@gmail.com>
2023-01-19 17:08:08 +0200

    soluion

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,11 +9,16 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverDeleteView,
+    DriverCreateView,
+    DriverUpdateView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    add_remove_driver,
 )
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -42,9 +47,26 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "cars/<int:pk>/add-remove-driver/",
+        add_remove_driver,
+        name="driver-add-remove"
+    ),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/<int:pk>/",
+         DriverDetailView.as_view(),
+         name="driver-detail"
+         ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/update",
+        DriverUpdateView.as_view(),
+        name="driver-update"
     ),
 ]
 
