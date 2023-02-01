a287e0f85986dd1321ba4e830813d66d9c788ccf
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-02-01 18:25:20 +0200

    Taxi service form and validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -8,13 +8,16 @@ from taxi.models import Driver, Car
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
         model = Driver
-        fields = "__all__"
+        fields = ("license_number",)
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
             raise ValidationError("License number length must be 8 characters")
-        if license_number[:3] != license_number[:3].upper():
+        if (
+                license_number[:3] != license_number[:3].upper()
+                or not all(element.isalpha() for element in license_number[:3])
+        ):
             raise ValidationError("First 3 elements must be in upper case")
         if not license_number[3:].isdigit():
             raise ValidationError("Last 5 elements must be digits")
