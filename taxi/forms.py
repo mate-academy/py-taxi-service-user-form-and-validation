41012af06966f7283f2ca0f1e397820e724d72e9
Taras Zelinskyi <taras.sv89@gmail.com>
2023-02-05 21:57:22 -0600

    solution

---
+++ taxi/forms.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
+
+from taxi.models import Driver, Car
+
+
+class DriverCreateForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("license_number",)
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = ("license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise ValidationError("License number should have 8 characters")
+        if license_number[:3] != license_number[:3].upper():
+            raise ValidationError("First 3 letters should be uppercase!")
+        for letter in license_number[3:]:
+            if not letter.isnumeric():
+                raise ValidationError("Last 5 characters should be digits!")
+        return license_number
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
