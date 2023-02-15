f489bdb0e9bb9a617237bdd04b8167be97d36372
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-02-15 23:06:39 +0800

    Solution 2.0

--- taxi/urls.py
+++ taxi/urls.py
@@ -15,7 +15,8 @@ from .views import (
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView, assign_delete_car,
+    ManufacturerDeleteView,
+    assign_delete_car,
 )
 
 urlpatterns = [
