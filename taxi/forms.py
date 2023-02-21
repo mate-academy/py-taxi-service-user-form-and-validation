d2507dd5945e60074a2d3591e9ae2b56afd0eee9
Anna Kabatova <kabatova.anna@ukr.net>
2023-02-21 18:53:08 +0200

    'initial'

---
+++ taxi/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+from taxi.models import Driver, Car
+
+
+class DriverCreationForm(UserCreationForm):
+	
+	class Meta(UserCreationForm.Meta):
+		model = Driver
+		fields = UserCreationForm.Meta.fields + ("license_number",)
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+	MAX_LEN = 8
+	
+	class Meta:
+		model = Driver
+		fields = ("license_number",)
+	
+	def clean_license_number(self):
+		license_number = self.cleaned_data["license_number"]
+		
+		if len(license_number) != DriverLicenseUpdateForm.MAX_LEN:
+			raise ValidationError(
+				f"Ensure that length of license number is < than {DriverLicenseUpdateForm.MAX_LEN}!"
+			)
+		
+		if license_number[:2].islower():
+			raise ValidationError(
+				"Ensure that your letters are uppercase!"
+			)
+		
+		if not isinstance(int(license_number[3:8]), int):
+			raise ValidationError(
+				"Your last 5 characters must be digits!"
+			)
+		
+		return license_number
+
+
+class CarForm(forms.ModelForm):
+	drivers = forms.ModelMultipleChoiceField(
+		queryset=get_user_model().objects.all(),
+		widget=forms.CheckboxSelectMultiple,
+		required=False
+	)
+	
+	class Meta:
+		model = Car
+		fields = "__all__"
--- taxi/urls.py
+++ taxi/urls.py
@@ -9,10 +9,12 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    ManufacturerDeleteView, DriverLicenseUpdateView, delete_assign_car,
 )
 
 urlpatterns = [
@@ -42,10 +44,18 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path(
+        "cars/<int:pk>/user/",
+        delete_assign_car,
+        name="car-delete-assign"
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/<int:pk>/update-license/", DriverLicenseUpdateView.as_view(), name="driver-license-update"),
 ]
 
 app_name = "taxi"
