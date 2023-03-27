f5a3c709da789dade721f7ff008c8f67f7aee5b3
Eugene <eugene.lot77@gmail.com>
2023-03-27 21:04:48 +0300

    Fixed for checklist

--- taxi/views.py
+++ taxi/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import DriverLicenseUpdateForm, CarForm
 from .models import Driver, Car, Manufacturer
