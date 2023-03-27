b2ca87ccd9365c658c319ba8ab4a747d63cd68b9
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-03-27 22:44:06 +0300

    DriverLicenseUpdateForm: got rid of redundal methods

--- taxi/forms.py
+++ taxi/forms.py
@@ -23,32 +23,17 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         model = Driver
         fields = ("license_number",)
 
-    @staticmethod
-    def check_upper(sequence: str) -> bool:
-        return sequence == sequence.upper()
-
-    @staticmethod
-    def check_alpha(sequence: list) -> bool:
-        return all([elem.isalpha() for elem in sequence])
-
     def clean_license_number(self):
         license_number = self.cleaned_data.get("license_number")
-        license_number_start = license_number[0:3]
 
         if len(license_number) != self.DRIVER_LICENSE_LEN:
             raise ValidationError("license number must contain 8 characters.")
         if not (
-            self.check_alpha(license_number_start)
-            and self.check_upper(license_number_start)
+            license_number[0:3].isalpha()
+            and license_number[0:3].isupper()
         ):
             raise ValidationError("first 3 characters must be capital letters")
 
-        if not "".join(license_number[3:]).isdigit():
+        if not license_number[3:].isdigit():
             raise ValidationError("last 5 characters must be digits")
         return license_number
-
-
-# class CarCreateForm(forms.ModelForm):
-#     drivers = forms.ModelMultipleChoiceField(
-#         queryset=get_user_model.
-#     )
