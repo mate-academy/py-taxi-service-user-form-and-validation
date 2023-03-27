cf5fb93163dc62d631d3fb5e744d0533d4e0c7e2
Vadym Bihun <bihunva@gmail.com>
2023-03-27 18:27:48 +0300

    flake8 again

--- taxi/validators.py
+++ taxi/validators.py
@@ -7,7 +7,7 @@ def validate_license_number(license_number):
         raise ValidationError("License must consist of 8 characters")
     if not license_number[:3].isupper() or not license_number[:3].isalpha():
         raise ValidationError(
-            "The first three characters in the license must be uppercase letters"
+            "The first three license's characters must be uppercase letters"
         )
     if not license_number[-5:].isdigit():
         raise ValidationError(
