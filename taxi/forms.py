4ef10fc2776a15651d2e568fd9842e5b26ba0b8f
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-03-29 18:37:47 +0200

    Fix driver update form and validate license number method

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,6 +6,15 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
+class DriverForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = "__all__"
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
 class DriverLicenseUpdateForm(forms.ModelForm):
 
     class Meta:
@@ -13,28 +22,22 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         fields = ["license_number"]
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-
-        if len(license_number) != 8:
-            raise ValidationError("length must be 8 symbols")
-        if (
-                not license_number[:3].isalpha()
-                or license_number[:2] != license_number[:2].upper()
-        ):
-            raise ValidationError("first 3 symbols must be in uppercase")
-        if not license_number[-5:].isdigit():
-            raise ValidationError("last 5 symbols must be digit")
-
-        return license_number
+        return validate_license_number(self.cleaned_data["license_number"])
 
 
-class DriverForm(DriverLicenseUpdateForm, UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = Driver
-        fields = "__all__"
+def validate_license_number(license_number: str) -> str:
 
-    def clean_license_number(self):
-        return super().clean_license_number()
+    if len(license_number) != 8:
+        raise ValidationError("length must be 8 symbols")
+    if (
+            not license_number[:3].isalpha()
+            or license_number[:3] != license_number[:3].upper()
+    ):
+        raise ValidationError("first 3 symbols must be in uppercase")
+    if not license_number[-5:].isdigit():
+        raise ValidationError("last 5 symbols must be digit")
+
+    return license_number
 
 
 class CarForm(forms.ModelForm):
