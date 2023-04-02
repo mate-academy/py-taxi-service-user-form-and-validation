34affaea781370955711c737aed758889011808d
kuznetsb <kuznetsbog@gmail.com>
2023-04-02 12:57:52 +0300

    Only solution found - separte validation fuction and use it in create and update models

--- taxi/forms.py
+++ taxi/forms.py
@@ -18,33 +18,17 @@ class CustomUserCreationForm(UserCreationForm):
         )
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        first_three = license_number[:3]
-        last_five = license_number[3:]
-
-        if len(license_number) != 8:
-            raise ValidationError(
-                "Ensure that license consists of 8 characters"
-            )
-
-        elif not first_three.isupper() or not first_three.isalpha():
-            raise ValidationError(
-                "Ensure that first three characters are uppercase letters"
-            )
-
-        elif not last_five.isdigit():
-            raise ValidationError(
-                "Ensure that last five characters are digits"
-            )
-
-        return license_number
+        return validate_license_number(self)
 
 
-class DriverLicenseUpdateForm(CustomUserCreationForm):
+class DriverLicenseUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Driver
         fields = ("license_number", )
+
+    def clean_license_number(self):
+        return validate_license_number(self)
 
 
 class CarForm(forms.ModelForm):
@@ -57,3 +41,25 @@ class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = "__all__"
+
+
+def validate_license_number(request):
+    license_number = request.cleaned_data["license_number"]
+    first_three = license_number[:3]
+    last_five = license_number[3:]
+
+    if len(license_number) != 8:
+        raise ValidationError(
+            "Ensure that license consists of 8 characters"
+        )
+    elif not first_three.isupper() or not first_three.isalpha():
+        raise ValidationError(
+            "Ensure that first three characters are uppercase letters"
+        )
+
+    elif not last_five.isdigit():
+        raise ValidationError(
+            "Ensure that last five characters are digits"
+        )
+
+    return license_number
