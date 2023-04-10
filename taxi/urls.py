b39be132176e4d6b85e9e12df247552b02deb980
YuriiBabiuk <118677914+YuriiBabiuk@users.noreply.github.com>
2023-04-10 18:11:21 +0300

    fix flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -60,8 +60,16 @@ urlpatterns = [
         name="driver-update"
     ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/update/", DriverUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
 ]
 
 app_name = "taxi"
