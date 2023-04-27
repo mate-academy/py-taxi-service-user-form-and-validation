65dde850c7eab7333ae9cf7587200ab318e388d4
Yura <yuraua5@gmail.com>
2023-04-27 16:45:39 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from .forms import DriverUserCreationForm, DriverLicenseUpdateForm, CarForm
 from .models import Driver, Car, Manufacturer
 
