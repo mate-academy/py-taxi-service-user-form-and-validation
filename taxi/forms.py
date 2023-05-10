5ae99a20cbf68e82575439895c74e1fff83a3d7a
kostya-kononenko <kkononenko3@gmail.com>
2023-05-10 12:44:53 +0300

    Solution1

--- taxi/forms.py
+++ taxi/forms.py
@@ -11,26 +11,26 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         model = Driver
         fields = ("license_number",)
 
-        def clean_license_number(self):
-            license_number = self.cleaned_data["license_number"]
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
 
-            if len(license_number) != 8:
-                raise ValidationError(
-                    "Licence must be of 8 characters."
-                )
-            if not license_number[:3].isupper():
-                raise ValidationError(
-                    "First 3 symbol must be uppercase."
-                )
-            if not license_number[:3].isalpha():
-                raise ValidationError(
-                    "First 3 symbol must be letters"
-                )
-            if not license_number[3:].isnumeric():
-                raise ValidationError(
-                    "Last 5 symbol must be numbers."
-                )
-            return license_number
+        if len(license_number) != 8:
+            raise ValidationError(
+                "Licence must be of 8 characters."
+            )
+        if not license_number[:3].isupper():
+            raise ValidationError(
+                "First 3 symbol must be uppercase."
+            )
+        if not license_number[:3].isalpha():
+            raise ValidationError(
+                "First 3 symbol must be letters"
+            )
+        if not license_number[3:].isnumeric():
+            raise ValidationError(
+                "Last 5 symbol must be numbers."
+            )
+        return license_number
 
 
 class CarForm(forms.ModelForm):
