a4480addecbe54b122a0d172bf6fcd5326d90a56
V Borovets <vborovets0@gmail.com>
2023-06-13 00:23:39 +0300

    End!

--- taxi/forms.py
+++ taxi/forms.py
@@ -14,9 +14,7 @@ class CheckLicenceNumberMixin(forms.ModelForm):
             raise ValidationError(
                 "License number must consist of 8 characters."
             )
-        if not license_number[0:3].isupper() and (
-                not license_number[0:3].isalpha()
-        ):
+        if not license_number[0:3].isalpha() or not license_number[0:3].isupper():
             raise ValidationError(
                 "First 3 characters must be uppercase letters"
             )
