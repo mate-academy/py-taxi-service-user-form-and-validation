8b74449a0ac311ec42686581d2c278d53fd17dc7
Halyna Petrova <1208petrova@gmail.com>
2023-06-13 14:59:20 +0300

    'Solution5'

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -86,7 +86,7 @@ class CarDriverUpdateView(LoginRequiredMixin, generic.CreateView):
 
     def post(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
-        car = Car.objects.get(id=pk)
+        car = get_object_or_404(Car, id=pk)
         if request.user in car.drivers.all():
             car.drivers.remove(request.user)
         else:
