75a539a905d915351d25b571a618b109d19a013e
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-07-07 10:34:35 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -22,14 +22,14 @@ def check_license_number(license_number):
 class DriverUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
-        fields = UserCreationForm.Meta.fields + ("license_number",)
+        fields = UserCreationForm.Meta.fields
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Driver
-        fields = ["license_number", "first_name", "last_name", "username"]
+        fields = ["license_number"]
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
