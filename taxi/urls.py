6a1b39cf5d3edc8954a6dad797a28853aa8354d2
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-19 00:37:49 +0300

    Solutin

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,13 +2,20 @@ from django.urls import path
 
 from .views import (
     index,
+
     CarListView,
     CarDetailView,
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
+    CarUpdateDriverView,
+
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverDeleteView,
+    DriverLicenseUpdateView,
+
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
@@ -37,15 +44,27 @@ urlpatterns = [
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
+
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("cars/<int:pk>/update-driver/", CarUpdateDriverView.as_view(), name="car-update-driver"),
+
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create/",
+         DriverCreateView.as_view(),
+         name="driver-create"),
+    path("drivers/<int:pk>/update/",
+         DriverLicenseUpdateView.as_view(),
+         name="driver-update"),
+    path("drivers/<int:pk>/delete/",
+         DriverDeleteView.as_view(),
+         name="driver-delete")
 ]
 
 app_name = "taxi"
