83c13d032ce422928c5b6c14e22955edfd529bbf
Tarasidze <dtar84@ukr.net>
2023-07-19 02:37:52 +0300

    Solution 1.01

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,8 +7,12 @@ from .views import (
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
+    car_update_driver,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverDeleteView,
+    DriverLicenseUpdateView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
@@ -38,14 +42,36 @@ urlpatterns = [
         name="manufacturer-delete",
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/detail/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "cars/<int:pk>/update-car-driver/",
+        car_update_driver,
+        name="car-update-driver"
     ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "drivers/<int:pk>/detail/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/driver-update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update"
+    )
 ]
 
 app_name = "taxi"
