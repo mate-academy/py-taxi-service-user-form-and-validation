ac3de7cf3b406be25039657d276441b1ee42dbe5
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-07-19 00:42:46 +0300

    Solutin

--- taxi/forms.py
+++ taxi/forms.py
@@ -10,7 +10,10 @@ class DriverCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = Driver
-        fields = UserCreationForm.Meta.fields + ("license_number", "first_name", "last_name")
+        fields = UserCreationForm.Meta.fields + (
+            "license_number",
+            "first_name",
+            "last_name")
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
@@ -22,11 +25,13 @@ class DriverLicenseUpdateForm(forms.ModelForm):
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
         if not license_number.isalnum():
-            raise ValidationError("License number must contain only letters and numbers!")
+            raise ValidationError("License number must contain "
+                                  "only letters and numbers!")
         if len(license_number) != 8:
             raise ValidationError("License number must be 8 symbols long!")
         if not (license_number[:3].isupper() and license_number[:3].isalpha()):
-            raise ValidationError("First 3 characters of license must be uppercase letters!")
+            raise ValidationError("First 3 characters of license "
+                                  "must be uppercase letters!")
         if not license_number[3:].isdigit():
             raise ValidationError("Last 5 characters must be digits!")
         return license_number
--- taxi/urls.py
+++ taxi/urls.py
@@ -50,7 +50,9 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("cars/<int:pk>/update-driver/", CarUpdateDriverView.as_view(), name="car-update-driver"),
+    path("cars/<int:pk>/update-driver/",
+         CarUpdateDriverView.as_view(),
+         name="car-update-driver"),
 
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
