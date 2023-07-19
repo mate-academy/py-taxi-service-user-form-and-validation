ff0cb290bedc3c7aa08795aff6e845c0ad4c1f7c
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-19 15:24:33 +0300

    fix problems

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,12 +6,6 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
-class DriverCreationForm(UserCreationForm):
-    class Meta:
-        model = Driver
-        fields = UserCreationForm.Meta.fields + ("license_number",)
-
-
 class DriverLicenseUpdateForm(forms.ModelForm):
 
     class Meta:
@@ -20,17 +14,31 @@ class DriverLicenseUpdateForm(forms.ModelForm):
 
     def clean_license_number(self):
         license_number = self.cleaned_data["license_number"]
-
         if len(license_number) != 8:
             raise ValidationError("license len should be equal to 8")
         print([64 < ord(char) < 91 for char in license_number[:3]])
-
         if not all(64 < ord(char) < 91 for char in license_number[:3]):
             raise ValidationError(
                 "first 3 license character should be uppercase letters"
             )
-
         if not license_number[-5:].isnumeric():
             raise ValidationError("last 5 license character should be numbers")
-
         return license_number
+
+
+class DriverCreationForm(UserCreationForm, DriverLicenseUpdateForm):
+    class Meta:
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("license_number",)
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
--- taxi/views.py
+++ taxi/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import DriverCreationForm, DriverLicenseUpdateForm
+from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 from .models import Driver, Car, Manufacturer
 
 
@@ -75,11 +75,13 @@ def car_detail_view(request, pk):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
