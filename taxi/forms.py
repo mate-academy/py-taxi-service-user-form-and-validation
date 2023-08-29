70c7d9d6345d6167951ed56d2bfebd7f4dce455a
Yaroslav Rybalka <markus9869@gmail.com>
2023-08-29 11:19:11 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -10,11 +10,8 @@ def validate_license_number(license_number):
     if len(license_number) != 8:
         raise ValidationError("Driver license must consist of 8 characters.")
 
-    if not license_number[:3].isupper():
+    if not license_number[:3].isupper() or not license_number[:3].isalpha():
         raise ValidationError("First 3 characters must be uppercase letters.")
-
-    if not license_number[:3].isalpha():
-        raise ValidationError("First 3 characters must be letters.")
 
     if not license_number[3:].isdigit():
         raise ValidationError("Last 5 characters must be digits.")
