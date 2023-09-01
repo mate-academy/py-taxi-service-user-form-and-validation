87b6bddc8c5f60336142ef521b1019e7e119cc82
Michael <manchester6913@gmail.com>
2023-09-01 05:49:54 -0400

    Solution4

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,17 +6,19 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
-def validate_license(self):
-    license_number = self.cleaned_data["license_number"]
+def validate_license(license_number: str) -> str:
     if len(license_number) != 8:
-        raise ValidationError("Must consist only of 8 characters")
-    first_3: str = license_number[:3]
-    if not (first_3.isalpha() and first_3.isupper()):
+        raise ValidationError(
+            "License number consist only of 8 characters"
+        )
+    if not license_number[:3].isalpha():
         raise ValidationError(
             "First 3 characters must be uppercase letters"
         )
-    if not license_number[-5:].isdigit():
-        raise ValidationError("Last 5 characters must be digits")
+    if not license_number[3:].isdigit():
+        raise ValidationError(
+            "Last 5 characters must be digits"
+        )
     return license_number
 
 
@@ -33,11 +35,14 @@ class DriverCreationForm(UserCreationForm):
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
-    license_number = forms.CharField(validators=[validate_license])
-
     class Meta:
-        model = get_user_model()
-        fields = ("license_number",)
+        model = Driver
+        fields = ("license_number", )
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        validate_license(license_number)
+        return license_number
 
 
 class CarForm(forms.ModelForm):
