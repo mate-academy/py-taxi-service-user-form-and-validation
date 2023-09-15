5113046f69c19bcb83a8c6eb5db97d13c1638ae5
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-15 20:28:59 +0200

    Refactor Change license form

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,6 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.core.exceptions import ValidationError
 
 from taxi.models import Driver, Car
 
@@ -8,7 +8,7 @@ from taxi.models import Driver, Car
 class DriverCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
-        model = Driver
+        model = get_user_model()
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "license_number"
         )
@@ -16,24 +16,8 @@ class DriverCreationForm(UserCreationForm):
 
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
-        model = Driver
+        model = get_user_model()
         fields = ("license_number",)
-
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-
-        if len(license_number) != 8:
-            raise ValidationError("License number must consist of 8 digits.")
-        if (
-                not license_number[:3].isalpha()
-                or not license_number[:3].isupper()
-        ):
-            raise ValidationError(
-                "License number must begin with 3 uppercase latin letters."
-            )
-        if not license_number[3:].isnumeric():
-            raise ValidationError("License number must end with 5 digits.")
-        return license_number
 
 
 class CarForm(forms.ModelForm):
