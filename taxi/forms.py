d45d658e9aa6c1c19e0c74dfbb5859ee08f79479
Ostap Ivakh <o.ivakh95@gmail.com>
2023-09-27 15:38:55 +0300

    'Solution'

---
+++ taxi/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from taxi.models import Car, Driver
+
+
+class DriverCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "license_number",
+        )
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ("license_number",)
+
+    license_number = forms.CharField(max_length=8, min_length=8)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data.get("license_number")
+
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License number must consist of 8 characters."
+            )
+
+        first_three_characters = license_number[:3]
+        if not all(char.isupper() for char in first_three_characters):
+            raise forms.ValidationError(
+                "First 3 characters must be uppercase letters."
+            )
+
+        if not license_number[-5:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits.")
+
+        return license_number
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
+        widgets = {
+            "drivers": forms.CheckboxSelectMultiple(),
+        }
