598994c4534a9a669fe94ec22080709ef975c6cf
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-09-29 01:02:26 +0300

    'Solution'

---
+++ taxi/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Driver, Car
+
+
+class LicenseNumberMixin:
+    def __init__(self):
+        self.cleaned_data = None
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License number must consist of 8 characters"
+            )
+        if not license_number[:3].isupper():
+            raise forms.ValidationError(
+                "First 3 characters must be uppercase letters"
+            )
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Last 5 characters must be digits")
+        return license_number
+
+
+class DriverCreationForm(UserCreationForm, LicenseNumberMixin):
+    license_number = forms.CharField(max_length=8, required=True)
+
+    class Meta:
+        model = Driver
+        fields = (
+            "username",
+            "first_name",
+            "last_name", "email",
+            "license_number"
+        )
+
+
+class DriverLicenseUpdateForm(forms.ModelForm, LicenseNumberMixin):
+    license_number = forms.CharField(max_length=8, required=True)
+
+    class Meta:
+        model = Driver
+        fields = ["license_number"]
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
--- taxi/urls.py
+++ taxi/urls.py
@@ -13,6 +13,11 @@ from .views import (
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    DriverCreateView,
+    DriverUpdateView,
+    DriverDeleteView,
+    assign_driver_to_car,
+    remove_driver_from_car,
 )
 
 urlpatterns = [
@@ -44,7 +49,30 @@ urlpatterns = [
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "cars/<int:car_id>/assign-driver/",
+        assign_driver_to_car,
+        name="assign-driver-to-car",
+    ),
+    path(
+        "cars/<int:car_id>/remove-driver/",
+        remove_driver_from_car,
+        name="remove-driver-from-car",
     ),
 ]
 
