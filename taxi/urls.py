9564e2ccb5eca1c2d43eeae13e517d1d9065ea5e
Igor <igornev83@gmail.com>
2023-10-02 16:07:19 +0300

    Tested

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,10 +9,14 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    LicenceUpdateView,
+    car_driver_update_view,
 )
 
 urlpatterns = [
@@ -41,11 +45,16 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/cardriverupdate/", car_driver_update_view, name="car-driver-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/<int:pk>/update/", LicenceUpdateView.as_view(), name="driver-update"),
+
 ]
 
 app_name = "taxi"
