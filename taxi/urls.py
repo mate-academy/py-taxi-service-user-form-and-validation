f31ec37a2b79c81454b14744bd6be85ef6a06270
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-10-10 13:06:34 +0300

    update taxi/urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -83,17 +83,17 @@ urlpatterns = [
         name="driver-detail"
     ),
     path(
-        "drivers/create",
+        "drivers/create/",
         DriverCreateView.as_view(),
         name="driver-create"
     ),
     path(
-        "drivers/<int:pk>/license-update",
+        "drivers/<int:pk>/license-update/",
         DriverLicenseUpdateView.as_view(),
         name="driver-update"
     ),
     path(
-        "drivers/<int:pk>/delete",
+        "drivers/<int:pk>/delete/",
         DriverDeleteView.as_view(),
         name="driver-delete"
     ),
