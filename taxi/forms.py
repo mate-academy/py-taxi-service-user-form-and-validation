05263593cae01d93cfbeb34a70a3debdf240f3a6
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-17 12:22:00 +0200

    Create license number validator and fix 'DriverCreationForm' and 'DriverLicenseUpdateForm'

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,39 +6,46 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
-def clean_license_number(cleaned_data):
+def validate_license_number(license_number: str):
     license_number_length = 8
-    license_number = cleaned_data["license_number"]
+
     if len(license_number) != license_number_length:
         raise ValidationError(
             "The license number should be "
             f"{license_number_length} characters long"
         )
+
     if not license_number[:3].isupper() or not license_number[:3].isalpha():
         raise ValidationError("First 3 symbols should be capital letters")
+
     if not license_number[3:].isdigit():
         raise ValidationError("Last 5 symbols should be numbers")
+
     return license_number
 
 
 class DriverCreationForm(UserCreationForm):
+    license_number = forms.CharField(
+        max_length=255,
+        validators=[validate_license_number]
+    )
+
     class Meta(UserCreationForm.Meta):
         model = Driver
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "license_number",
         )
 
-    def clean(self):
-        clean_license_number(self.cleaned_data)
-
 
 class DriverLicenseUpdateForm(forms.ModelForm):
+    license_number = forms.CharField(
+        max_length=255,
+        validators=[validate_license_number]
+    )
+
     class Meta:
         model = Driver
         fields = ("license_number", )
-
-    def clean(self):
-        clean_license_number(self.cleaned_data)
 
 
 class CarForm(forms.ModelForm):
