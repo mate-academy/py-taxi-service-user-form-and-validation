642e6ea22c70a0902f9e9a0850a359704fdd2cbd
Artem <hartem472@gmail.com>
2023-11-08 16:42:49 +0100

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/update-license/",
         DriverLicenseUpdateView.as_view(),
-        name="driver-license-update"
+        name="driver-update"
     ),
 ]
 
