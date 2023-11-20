9f49ff871199f8ef43ce4485201d04e037fc061a
sasha <magazynuniversam@gmail.com>
2023-11-20 18:49:20 +0200

    add validations to form class

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.core.validators import RegexValidator
 
 from taxi.models import Driver, Car
 
@@ -23,6 +24,19 @@ class CarCreateForm(forms.ModelForm):
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
+    license_number_validator = RegexValidator(
+        regex=r'^[A-Z]{3}\d{5}$',
+        message='License number must consist of 3 uppercase letters followed by 5 digits.',
+        code='invalid_license_number'
+    )
+
+    license_number = forms.CharField(validators=[license_number_validator])
+
     class Meta:
         model = Driver
         fields = ("license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data['license_number']
+
+        return license_number
