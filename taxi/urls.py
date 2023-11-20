227e4b868507892d77947ecd850809b197847e9a
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-11-20 20:45:33 +0200

    Fixed flake

--- taxi/urls.py
+++ taxi/urls.py
@@ -55,7 +55,9 @@ urlpatterns = [
         "drivers/crete", DriverCreateView.as_view(), name="driver-create"
     ),
     path(
-        "drivers/<int:pk>/delete", DriverDeleteView.as_view(), name="driver-delete"
+        "drivers/<int:pk>/delete",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
     ),
     path(
         "drivers/<int:pk>licence-update",
