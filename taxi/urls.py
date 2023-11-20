9aa8f847fb3d36384ba74ffeecbb4f80404d0c02
Severyn Krok <sevkrok@gmail.com>
2023-11-20 19:58:48 +0200

    Implement py-taxi-service-user-form-and-validation

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,14 +9,18 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverUpdateView,
+    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    ManufacturerDeleteView, assign_driver_to_car, delete_driver_from_car,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
@@ -37,14 +41,37 @@ urlpatterns = [
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
+
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path(
+        "cars/<int:pk>/driver-assign/",
+        assign_driver_to_car,
+        name="assign-driver-to-car"
+    ),
+    path(
+        "cars/<int:pk>/driver-delete/",
+        delete_driver_from_car,
+        name="delete-driver-from-car"
+    ),
+
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update"
     ),
 ]
 
