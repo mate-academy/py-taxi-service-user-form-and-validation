a3ddb309470a3cb2f083ef5989f8f7c8cb419b4f
Denys <den.prislipskyi@gmail.com>
2023-11-20 16:54:12 +0100

    fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -68,10 +68,10 @@ urlpatterns = [
          name="driver-update"),
     path("cars/<int:pk>/add-driver/",
          car_add_driver,
-         name="car_add_driver"),
+         name="driver-add-car"),
     path("cars/<int:pk>/delete-driver/",
          car_delete_driver,
-         name="car_delete_driver"),
+         name="driver-delete-car"),
 ]
 
 app_name = "taxi"
