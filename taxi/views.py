0171f8bbac0c84fa41ae1e173c9d8f277a0e4c55
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-11-20 09:04:54 +0200

    Deleted unnecessary imports

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
