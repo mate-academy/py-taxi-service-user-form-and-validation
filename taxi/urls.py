f430c1380a625369cb2b5fa61d1e3394eb4153f0
Roman Muraviov <5079447@gmail.com>
2023-11-26 20:38:41 +0200

    Revert "Solution"
    This reverts commit e5eee2a9522ce954cda5c447b457680bfe97b497.

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,14 +9,10 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
-    DriverCreateView,
-    DriverLicenseUpdateView,
-    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
-    toggle_assign_to_car,
 )
 
 urlpatterns = [
@@ -41,52 +37,15 @@ urlpatterns = [
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete",
     ),
-    path("cars/",
-         CarListView.as_view(),
-         name="car-list"
-         ),
-    path("cars/<int:pk>/",
-         CarDetailView.as_view(),
-         name="car-detail"
-         ),
-    path("cars/create/",
-         CarCreateView.as_view(),
-         name="car-create"
-         ),
-    path("cars/<int:pk>/update/",
-         CarUpdateView.as_view(),
-         name="car-update"
-         ),
-    path("cars/<int:pk>/delete/",
-         CarDeleteView.as_view(),
-         name="car-delete"
-         ),
-    path("drivers/",
-         DriverListView.as_view(),
-         name="driver-list"
-         ),
-    path("drivers/<int:pk>/",
-         DriverDetailView.as_view(),
-         name="driver-detail"
-         ),
-    path("drivers/create/",
-         DriverCreateView.as_view(),
-         name="driver-create"
-         ),
-    path("drivers_license/<int:pk>/update/",
-         DriverLicenseUpdateView.as_view(),
-         name="driver-update"
-         ),
-    path("drivers/<int:pk>delete/",
-         DriverDeleteView.as_view(),
-         name="driver-delete"
-         ),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("cars/create/", CarCreateView.as_view(), name="car-create"),
+    path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "cars/<int:pk>/toggle-assign/",
-        toggle_assign_to_car,
-        name="toggle-car-assign",
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-
 ]
 
 app_name = "taxi"
