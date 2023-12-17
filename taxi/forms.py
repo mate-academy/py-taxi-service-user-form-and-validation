4ed733e9f03e701304c2b1f792a962986ff165b2
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-12-17 17:32:09 +0000

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -7,28 +7,31 @@ from django.forms import CheckboxSelectMultiple
 from taxi.models import Driver, Car
 
 
+def clean_license_number(self):
+    license_number = self.cleaned_data["license_number"]
+    if len(license_number) != 8:
+        raise ValidationError("license number should be 8 characters")
+    if license_number[:3] != str(license_number[0:3]).upper():
+        raise ValidationError("first 3 characters have to be uppercase")
+    for letter in license_number[:3]:
+        try:
+            int(letter)
+            raise ValidationError("first 3 characters have to be letters")
+        except ValueError:
+            continue
+    try:
+        int(license_number[-5:])
+        return license_number
+    except ValueError:
+        raise ValidationError("Last 5 characters have to be digits")
+
+
 class UserCreationForm(UserCreationForm):
     class Meta:
         model = Driver
         fields = UserCreationForm.Meta.fields + ("license_number",)
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise ValidationError("license number should be 8 characters")
-        if license_number[:3] != str(license_number[:3]).upper():
-            raise ValidationError("first 3 characters have to be uppercase")
-        for letter in license_number[:3]:
-            try:
-                int(letter)
-                raise ValidationError("first 3 characters have to be letters")
-            except ValueError:
-                continue
-        try:
-            int(license_number[-5:])
-            return license_number
-        except ValueError:
-            raise ValidationError("Last 5 characters have to be digits")
+    clean_license_number = clean_license_number
 
 
 class CarCreationForm(forms.ModelForm):
@@ -48,21 +51,4 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         model = Driver
         fields = ("license_number",)
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise ValidationError("license number should be 8 characters")
-        if license_number[:3] != str(license_number[0:3]).upper():
-            raise ValidationError("first 3 characters have to be uppercase")
-        for letter in license_number[:3]:
-            try:
-                int(letter)
-                raise ValidationError("first 3 characters have to be letters")
-            except ValueError:
-                continue
-        try:
-
-            int(license_number[-5:])
-            return license_number
-        except ValueError:
-            raise ValidationError("Last 5 characters have to be digits")
+    clean_license_number = clean_license_number
