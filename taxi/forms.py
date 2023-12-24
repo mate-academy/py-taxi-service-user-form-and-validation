62c6e00946ad1d65e525a27b39dc83c85fac67b2
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-12-24 16:51:24 +0000

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -7,26 +7,26 @@ from django.forms import CheckboxSelectMultiple
 from taxi.models import Driver, Car
 
 
-def clean_license_number(self):
-    license_number = self.cleaned_data["license_number"]
-    if len(license_number) != 8:
-        raise ValidationError("license number should be 8 characters")
-    for letter in license_number[:3]:
-        if letter < "A" or letter > "Z":
+class CleanMixin:
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise ValidationError("license number should be 8 characters")
+        is_digit = str.isdigit
+        if any(map(is_digit, license_number[:3])):
+            raise ValidationError(
+                "first 3 characters have to be letters")
+        if license_number[:3].upper() != license_number[:3]:
             raise ValidationError(
                 "first 3 characters have to be uppercase letters")
-    for letter in license_number[-5:]:
-        if letter < "1" or letter > "9":
+        if not all(map(is_digit, license_number[-5:])):
             raise ValidationError("Last 5 characters have to be digits")
-    return license_number
+        return license_number
 
-
-class UserCreationForm(UserCreationForm):
+class UserCreationForm(CleanMixin, UserCreationForm):
     class Meta:
         model = Driver
         fields = UserCreationForm.Meta.fields + ("license_number",)
-
-    clean_license_number = clean_license_number
 
 
 class CarCreationForm(forms.ModelForm):
@@ -41,9 +41,7 @@ class CarCreationForm(forms.ModelForm):
         fields = "__all__"
 
 
-class DriverLicenseUpdateForm(forms.ModelForm):
+class DriverLicenseUpdateForm(CleanMixin, forms.ModelForm):
     class Meta:
         model = Driver
         fields = ("license_number",)
-
-    clean_license_number = clean_license_number
