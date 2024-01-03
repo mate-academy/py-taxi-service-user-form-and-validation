26403a057a5a2405d5ac13f3a0632ec8a766070c
Даня Пономаренко <you@example.com>
2024-01-03 20:27:34 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,19 +5,34 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver
 
 
-class DriverCreateForm(UserCreationForm, forms.ModelForm):
+def validate_license_number(license_number) -> str:
+    for letter in license_number[:3]:
+        if letter not in "QWERTYUIOPASDFGHJKLZXCVBNM":
+            raise ValidationError("Ensure the value of first"
+                                  " must be 3 uppercase letter")
+    for num in license_number[3:]:
+        if num not in "1234567890":
+            raise ValidationError("Ensure the value"
+                                  " of last 5 symbols must be numbers")
+    if len(license_number) != 8:
+        raise ValidationError("Ensure the lenght of license number must be 8")
+    return license_number
+
+
+class DriverCreateForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = Driver
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number")
+        fields = (UserCreationForm.Meta.fields
+                  + ("first_name", "last_name", "license_number"))
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        for letter in license_number[:3]:
-            if letter not in "QWERTYUIOPASDFGHJKLZXCVBNM":
-                raise ValidationError(f"Ensure the value of first must be 3 uppercase letter")
-        for num in license_number[3:]:
-            if num not in "1234567890":
-                raise ValidationError(f"Ensure the value of last 5 symbols must be numbers")
-        if len(license_number) != 8:
-            raise ValidationError(f"Ensure the lenght of license number must be 8")
-        return license_number
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Driver
+        fields = ["license_number"]
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
--- taxi/urls.py
+++ taxi/urls.py
@@ -12,7 +12,11 @@ from .views import (
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
-    ManufacturerDeleteView, DriverCreateView, DriverUpdateView, DriverDeleteView, assign_to_car,
+    ManufacturerDeleteView,
+    DriverCreateView,
+    DriverUpdateView,
+    DriverDeleteView,
+    assign_to_car,
 )
 
 urlpatterns = [
@@ -44,11 +48,17 @@ urlpatterns = [
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
     ),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/update/", DriverUpdateView.as_view(), name="driver-update"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
+    path("drivers/<int:pk>/update/",
+         DriverUpdateView.as_view(),
+         name="driver-update"),
+    path("drivers/<int:pk>/delete/",
+         DriverDeleteView.as_view(),
+         name="driver-delete"),
     path(
         "cars/<int:pk>/assign-driver/",
         assign_to_car,
