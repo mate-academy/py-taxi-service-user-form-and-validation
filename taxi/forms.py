3213f5a72d1c501cfda6aae06f9bc11f6666c515
Anton <anton380969658858@gmail.com>
2024-01-05 17:16:54 +0200

    solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -17,68 +17,46 @@ class DriverCreationForm(UserCreationForm):
         )
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-
-        if len(license_number) != DriverLicenseUpdateForm.LICENSE_NUMBER_LEN:
-            raise ValidationError(
-                f"Ensure that that length of license number is "
-                f"{DriverLicenseUpdateForm.LICENSE_NUMBER_LEN}"
-            )
-
-        if (not license_number[
-                :DriverLicenseUpdateForm.UPPERCASE_NUM].isupper()
-                or not license_number[
-                    :DriverLicenseUpdateForm.UPPERCASE_NUM].isalpha()):
-            raise ValidationError(
-                f"Ensure that first {DriverLicenseUpdateForm.UPPERCASE_NUM} "
-                f"characters are uppercase letters"
-            )
-
-        if not license_number[
-                DriverLicenseUpdateForm.UPPERCASE_NUM:].isdigit():
-            raise ValidationError(
-                f"Ensure that last {DriverLicenseUpdateForm.DIGIT_NUM} "
-                f"characters are digits"
-            )
-
-        return license_number
+        return clean_license_number(self.cleaned_data["license_number"])
 
 
-class DriverLicenseUpdateForm(UserChangeForm):
+class DriverLicenseUpdateForm(forms.ModelForm):
     LICENSE_NUMBER_LEN = 8
     UPPERCASE_NUM = 3
     DIGIT_NUM = 5
 
-    class Meta(UserChangeForm.Meta):
+    class Meta:
         model = get_user_model()
         fields = ("license_number",)
 
     def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
+        return clean_license_number(self.cleaned_data["license_number"])
 
-        if len(license_number) != DriverLicenseUpdateForm.LICENSE_NUMBER_LEN:
-            raise ValidationError(
-                f"Ensure that that length of license number is "
-                f"{DriverLicenseUpdateForm.LICENSE_NUMBER_LEN}"
-            )
 
-        if (not license_number[
-                :DriverLicenseUpdateForm.UPPERCASE_NUM].isupper()
-                or not license_number[
-                :DriverLicenseUpdateForm.UPPERCASE_NUM].isalpha()):
-            raise ValidationError(
-                f"Ensure that first {DriverLicenseUpdateForm.UPPERCASE_NUM} "
-                f"characters are uppercase letters"
-            )
+def clean_license_number(license_number):
+    if len(license_number) != DriverLicenseUpdateForm.LICENSE_NUMBER_LEN:
+        raise ValidationError(
+            f"Ensure that that length of license number is "
+            f"{DriverLicenseUpdateForm.LICENSE_NUMBER_LEN}"
+        )
 
-        if not license_number[
-                DriverLicenseUpdateForm.UPPERCASE_NUM:].isdigit():
-            raise ValidationError(
-                f"Ensure that last {DriverLicenseUpdateForm.DIGIT_NUM} "
-                f"characters are digits"
-            )
+    if (not license_number[
+            :DriverLicenseUpdateForm.UPPERCASE_NUM].isupper()
+            or not license_number[
+            :DriverLicenseUpdateForm.UPPERCASE_NUM].isalpha()):
+        raise ValidationError(
+            f"Ensure that first {DriverLicenseUpdateForm.UPPERCASE_NUM} "
+            f"characters are uppercase letters"
+        )
 
-        return license_number
+    if not license_number[
+            DriverLicenseUpdateForm.UPPERCASE_NUM:].isdigit():
+        raise ValidationError(
+            f"Ensure that last {DriverLicenseUpdateForm.DIGIT_NUM} "
+            f"characters are digits"
+        )
+
+    return license_number
 
 
 class CarForm(forms.ModelForm):
