e052a242aacf92fc9e966c7dfbaac782a2080dc5
Vadym Tiron <tironvadim1@gmail.com>
2024-01-06 18:58:36 +0200

    'Solution'

--- taxi/forms.py
+++ taxi/forms.py
@@ -19,22 +19,22 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         model = Driver
         fields = ("license_number",)
 
-        def clean_license_number(self):
-            license_number = self.cleaned_data["license_number"]
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
 
-            if len(license_number) != 8:
-                raise forms.ValidationError(
-                    "License number must consist of 8 characters.")
+        if len(license_number) != 8:
+            raise forms.ValidationError(
+                "License number must consist of 8 characters.")
 
-            if not license_number[:3].isupper():
-                raise forms.ValidationError(
-                    "First 3 characters must be uppercase letters.")
+        if not license_number[:3].isupper() or not license_number[:3].isalpha():
+            raise forms.ValidationError(
+                "First 3 characters must be uppercase letters.")
 
-            if not license_number[3:].isdigit():
-                raise forms.ValidationError(
-                    "Last 5 characters must be digits.")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError(
+                "Last 5 characters must be digits.")
 
-            return license_number
+        return license_number
 
 
 class CarCreationForm(forms.ModelForm):
