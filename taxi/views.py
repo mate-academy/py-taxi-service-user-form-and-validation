83e6727b2440fa6c0e76865a2263993fab24d168
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-01-24 22:54:39 +0200

    fixn

--- taxi/views.py
+++ taxi/views.py
@@ -66,14 +66,12 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    # fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
     form_class = CarForm
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    # fields = "__all__"
     success_url = reverse_lazy("taxi:car-list")
     form_class = CarForm
 
