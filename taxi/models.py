0fe9a8c39c48c1f9aa6c80abe3fc3d5160e7ba30
MaxymChyncha <max.chyncha@gmail.com>
2024-02-27 21:27:36 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,4 @@
+from django.core import validators
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
@@ -15,7 +16,21 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(
+        max_length=8,
+        unique=True,
+        validators=[
+            validators.RegexValidator(
+                regex="^[A-Z]{3}\\d{5}$",
+                message=(
+                    "Your license number should "
+                    "start with 3 uppercase letters "
+                    "and end with 5 digits."
+                ),
+                code="wrong license number"
+            )
+        ]
+    )
 
     class Meta:
         verbose_name = "driver"
--- taxi/urls.py
+++ taxi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     index,
+    assign_driver_to_car,
     CarListView,
     CarDetailView,
     CarCreateView,
@@ -9,10 +10,13 @@ from .views import (
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    DriverLicenseUpdateView,
 )
 
 urlpatterns = [
@@ -42,10 +46,28 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path(
+        "cars/<int:pk>/assign-driver/",
+        assign_driver_to_car,
+        name="car-assign-driver"
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path(
+        "drivers/create/", DriverCreateView.as_view(), name="driver-create"
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
+    ),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update"
+    )
 ]
 
 app_name = "taxi"
