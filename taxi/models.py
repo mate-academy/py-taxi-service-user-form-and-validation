fbbabe450e55e531ff0ca7b5c31cc5b5f8b87ee0
barabarinov <barabarinov@gmail.com>
2024-02-28 00:21:16 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from django.core.validators import RegexValidator
 
 
 class Manufacturer(models.Model):
@@ -15,7 +16,19 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(
+        max_length=8,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"^[A-Z]{3}\d{5}$",
+                message=(
+                    "Input format is incorrect, "
+                    "please follow the example: `AAA12345`"
+                ),
+            )
+        ],
+    )
 
     class Meta:
         verbose_name = "driver"
