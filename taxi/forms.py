c32e8654fab91102d9726569ba12c25aba51240f
Максим <www.depaton@gmail.com>
2024-02-28 03:44:45 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,37 +6,28 @@ from django import forms
 from taxi.models import Driver, Car
 
 
-class DriverCreationForm(forms.ModelForm):
-    LENGTH_OF_LICENSE = 8
+class DriverCreationForm(UserCreationForm):
+    LICENCE_LEN = 8
 
-    def clean_license_number(self) -> str:
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != DriverCreationForm.LENGTH_OF_LICENSE:
-            raise ValidationError(
-                f"License number must be "
-                f"{DriverCreationForm.LENGTH_OF_LICENSE}"
-                f" characters length."
-            )
-        if (
-            not license_number[:3].isupper()
-            or not license_number[:3].isalpha()
-        ):
-            raise ValidationError(
-                "First 3 characters must be uppercase letters."
-            )
-        if not license_number[-5:].isdigit():
-            raise ValidationError("Last 5 characters must be digits.")
+    class Meta(UserCreationForm.Meta):
+        model = Driver
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number")
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data.get("license_number")
+        if len(license_number) != 8:
+            raise ValidationError(f"The license number must be {self.LICENCE_LEN} characters long.")
+        if not license_number[:4].isupper():
+            raise ValidationError("The license number must start with 3 UPPER CASE LETTERS")
+        if not license_number[4:].isdigit():
+            raise ValidationError("The license number must end with 5 NUMBERS")
         return license_number
 
 
 class DriverLicenseUpdateForm(UserChangeForm):
     class Meta:
         model = Driver
-        fields = UserCreationForm.Meta.fields + (
-            "first_name",
-            "last_name",
-            "license_number"
-        )
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number")
 
 
 class CarForm(forms.ModelForm):
