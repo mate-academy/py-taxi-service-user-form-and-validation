462a67b2de634a301d0b0a971c5aa21fa6cc62ca
Artem Hryshko <artemggg2222@gmail.com>
2024-03-01 18:56:57 +0200

    Solution1

--- taxi/forms.py
+++ taxi/forms.py
@@ -9,16 +9,23 @@ def validate_license_number(value):
     license_number = value
 
     if len(license_number) != 8:
-        raise ValidationError("License_number should consist only of 8 characters")
+        raise ValidationError(
+            "License_number should consist only of 8 characters"
+        )
     if not license_number[:3].isupper() or not license_number[:3].isalpha():
-        raise ValidationError("First 3 characters of license_number should be uppercase letters")
+        raise ValidationError(
+            "First 3 characters of license_number should be uppercase letters"
+        )
     if not license_number[3:].isdigit():
         raise forms.ValidationError("Last 5 characters must be digits.")
     return license_number
 
 
 class DriverCreationForm(UserCreationForm):
-    license_number = forms.CharField(max_length=8, validators=[validate_license_number])
+    license_number = forms.CharField(
+        max_length=8,
+        validators=[validate_license_number]
+    )
 
     class Meta(UserCreationForm.Meta):
         model = Driver
@@ -26,7 +33,10 @@ class DriverCreationForm(UserCreationForm):
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
-    license_number = forms.CharField(max_length=8, validators=[validate_license_number])
+    license_number = forms.CharField(
+        max_length=8,
+        validators=[validate_license_number]
+    )
 
     class Meta:
         model = Driver
--- taxi/urls.py
+++ taxi/urls.py
@@ -53,12 +53,20 @@ urlpatterns = [
         "drivers/create", DriverCreateView.as_view(), name="driver-create"
     ),
     path(
-        "drivers/<int:pk>/delete", DriverDeleteView.as_view(), name="driver-delete"
+        "drivers/<int:pk>/delete",
+        DriverDeleteView.as_view(),
+        name="driver-delete"
     ),
     path(
-        "drivers/<int:pk>/update", DriverUpdateView.as_view(), name="driver-update"
+        "drivers/<int:pk>/update",
+        DriverUpdateView.as_view(),
+        name="driver-update"
     ),
-    path('car/<int:car_id>/assign-driver/', assign_driver_to_car, name='assign_driver_to_car'),
+    path(
+        "car/<int:car_id>/assign-driver/",
+        assign_driver_to_car,
+        name="assign_driver_to_car"
+    ),
 ]
 
 app_name = "taxi"
