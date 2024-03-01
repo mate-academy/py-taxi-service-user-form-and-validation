65da5564f0385412963f65d15759e420b3947f6c
barabarinov <barabarinov@gmail.com>
2024-03-01 18:57:48 +0200

    fixed

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.core.exceptions import ValidationError
 
 from taxi.models import Driver, Car
 
@@ -8,7 +9,7 @@ from taxi.models import Driver, Car
 class DriverCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
-        model = Driver
+        model = get_user_model()
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "license_number",
         )
@@ -28,5 +29,21 @@ class CarForm(forms.ModelForm):
 
 class DriverLicenseUpdateForm(forms.ModelForm):
     class Meta:
-        model = Driver
+        model = get_user_model()
         fields = ("license_number", )
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
+
+def validate_license_number(
+    license_number: str,
+) -> str | ValidationError:
+    if len(license_number) != 8:
+        raise ValidationError("License number should consist of 8 characters")
+    elif not license_number[:3].isupper() or not license_number[:3].isalpha():
+        raise ValidationError("First 3 characters should be uppercase letters")
+    elif not license_number[3:].isdigit():
+        raise ValidationError("Last 5 characters should be digits")
+
+    return license_number
--- taxi/models.py
+++ taxi/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
-from django.core.validators import RegexValidator
 
 
 class Manufacturer(models.Model):
@@ -16,20 +15,7 @@ class Manufacturer(models.Model):
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(
-        max_length=8,
-        unique=True,
-        validators=[
-            RegexValidator(
-                regex=r"^[A-Z]{3}\d{5}$",
-                message=(
-                    "Input format is incorrect, "
-                    "please follow the example: "
-                    "`AAA12345`"
-                ),
-            )
-        ],
-    )
+    license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
         verbose_name = "driver"
