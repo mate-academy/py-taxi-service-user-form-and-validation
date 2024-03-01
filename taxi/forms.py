8f985ff6302a70ad3299ac1707105a9a3c0677bc
Artem Milchenko <pl301078map2@gmail.com>
2024-03-01 11:08:54 +0200

    fix comments

--- taxi/forms.py
+++ taxi/forms.py
@@ -9,8 +9,8 @@ from taxi.models import Driver, Car
 def validate_license_number(license_number):
     if len(license_number) != 8:
         raise ValidationError("License number must be 8 characters long.")
-    if not license_number[:3].isalpha() or \
-            not license_number[:3].isupper():
+    if (not license_number[:3].isalpha() or
+            not license_number[:3].isupper()):
         raise ValidationError(
             "First 3 characters must be uppercase letters."
         )
