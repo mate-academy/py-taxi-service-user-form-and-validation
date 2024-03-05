e70613d82818f3b88a443a4a93dafc15f33d3f29
Sikach Illia <sikatch.ilja@gmail.com>
2024-03-06 00:01:44 +0200

    'flake8'

--- taxi/forms.py
+++ taxi/forms.py
@@ -17,13 +17,14 @@ class DriverLicenseUpdateForm(ModelForm):
         fields = ("license_number",)
 
     def clean_license_number(self):
-        license_number = self.cleaned_data['license_number']
+        license_number = self.cleaned_data["license_number"]
         if len(license_number) != 8:
-            raise ValidationError("License number should consist of 8 characters")
-        elif not license_number[:3].isupper() or not license_number[:3].isalpha():
-            raise ValidationError("First 3 characters should be uppercase letters")
+            raise ValidationError("License should consist of 8 characters")
+        elif (not license_number[:3].isupper()
+              or not license_number[:3].isalpha()):
+            raise ValidationError("First 3 chars should be uppercase letters")
         elif not license_number[3:].isdigit():
-            raise ValidationError("Last 5 characters should be digits")
+            raise ValidationError("Last 5 chars should be digits")
         return license_number
 
 
