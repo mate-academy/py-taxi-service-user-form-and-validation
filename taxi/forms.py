c13c6265f3a51c67cb30c40ff7a0978e586de267
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-03-05 09:18:26 +0200

    fix license number validation

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,44 +1,41 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
 from taxi.models import Car
 
-VALIDATION_REGEX = r"^[A-Z]{3}[0-9]{5}$"
-VALIDATION_MESSAGE = (
-    "Consist only of 8 characters\n"
-    "First 3 characters are uppercase letters\n"
-    "Last 5 characters are digits\n"
-)
+
+def validate_license_number(license_number):
+    if len(license_number) != 8:
+        raise ValidationError("Consist only of 8 characters")
+    elif not license_number[:3].isalpha() or not license_number[:3].isupper():
+        raise ValidationError("First 3 characters are uppercase letters")
+    elif not license_number[3:].isdigit():
+        raise ValidationError("Last 5 characters are digits")
+
+    return license_number
 
 
 class DriverCreationForm(UserCreationForm):
-    license_number = forms.CharField(
-        required=True,
-        validators=[
-            RegexValidator(VALIDATION_REGEX, VALIDATION_MESSAGE)
-        ]
-    )
-
     class Meta:
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name", "license_number",
         )
 
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
+
 
 class DriverLicenseUpdateForm(forms.ModelForm):
-    license_number = forms.CharField(
-        required=True,
-        validators=[
-            RegexValidator(VALIDATION_REGEX, VALIDATION_MESSAGE)
-        ]
-    )
-
     class Meta:
         model = get_user_model()
         fields = ("license_number",)
+
+    def clean_license_number(self):
+        return validate_license_number(self.cleaned_data["license_number"])
 
 
 class CarForm(forms.ModelForm):
