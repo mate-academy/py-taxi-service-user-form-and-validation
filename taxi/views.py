8ed0d54b2ac26e2cc5d21e9be4a9ffcdb8f9d70d
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-03-08 08:57:35 +0200

    fixed car_assignment function

--- taxi/views.py
+++ taxi/views.py
@@ -114,5 +114,6 @@ def car_assignment(request: HttpRequest, pk: int) -> HttpResponse:
     car = Car.objects.get(id=pk)
     if request.user in car.drivers.all():
         car.drivers.remove(request.user)
-    car.drivers.add(request.user)
+    else:
+        car.drivers.add(request.user)
     return redirect("taxi:car-detail", pk=pk)
