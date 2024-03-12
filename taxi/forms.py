3c311a4b68a439fa271cf8ce355b6b24f322e2b3
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2024-03-12 10:58:24 +0100

    Update forms.py

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,66 +5,38 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
-class DriverForm(forms.ModelForm):
+class LicenseNumberMixin:
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise ValidationError(
+                "license number must consist only 8 characters"
+            )
+        if license_number[:3] != license_number[:3].upper():
+            raise ValidationError(
+                "First 3 characters must be uppercase letters"
+            )
+        if not all(element.isalpha() for element in license_number[:3]):
+            raise ValidationError(
+                "First 3 character must be letters"
+            )
+        if not all(element.isdigit() for element in license_number[3:]):
+            raise ValidationError(
+                "Last 5 characters must be digits"
+            )
+        return license_number
+
+
+class DriverForm(forms.ModelForm, LicenseNumberMixin):
     class Meta:
         model = Driver
         fields = ("first_name", "last_name", "license_number")
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise ValidationError(
-                "license number must consist only 8 characters"
-            )
-        if (
-                license_number[:3] != license_number[:3].upper()
-        ):
-            raise ValidationError(
-                "First 3 characters must be uppercase letters"
-            )
-        if not all(element.isalpha() for element in license_number[:3]):
-            raise ValidationError(
-                "First 3 character must be letters"
-            )
 
-        if not all(
-                element.isdigit() for element in license_number[3:]
-        ):
-            raise ValidationError(
-                "Last 5 characters must be digits"
-            )
-        return license_number
-
-
-class DriverLicenseUpdateForm(forms.ModelForm):
+class DriverLicenseUpdateForm(forms.ModelForm, LicenseNumberMixin):
     class Meta:
         model = Driver
         fields = ("license_number", )
-
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-        if len(license_number) != 8:
-            raise ValidationError(
-                "license number must consist only 8 characters"
-            )
-        if (
-            license_number[:3] != license_number[:3].upper()
-        ):
-            raise ValidationError(
-                "First 3 characters must be uppercase letters"
-            )
-        if not all(element.isalpha() for element in license_number[:3]):
-            raise ValidationError(
-                "First 3 character must be letters"
-            )
-
-        if not all(
-            element.isdigit() for element in license_number[3:]
-        ):
-            raise ValidationError(
-                "Last 5 characters must be digits"
-            )
-        return license_number
 
 
 class CarForm(forms.ModelForm):
