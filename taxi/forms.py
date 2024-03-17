464b98e1f81b2cfd841f70744cf04872ee2abd77
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-17 22:42:54 +0200

    fixed files

--- taxi/forms.py
+++ taxi/forms.py
@@ -1,10 +1,12 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from .models import Driver, Car
 
 
 class DriverCreationForm(UserCreationForm):
+    license_number = forms.CharField(max_length=8)
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = UserCreationForm.Meta.fields + (
